b3810c5e138c10f4fdaec43b717c05b9ad4da752
Mohamed Ramadan <mohameda7maa@gmail.com>
2024-08-02 17:59:16 +0300

    finished login form

--- components/auth/Header.tsx
+++ components/auth/Header.tsx
@@ -11,7 +11,7 @@ interface HeaderProps{
 }
 export const Header = ({label} : HeaderProps) =>{
     return <div className="w-full flex flex-col items-center justify-center gap-y-4">
-        <h1 className={cn("text-3xl font-semibold", font.className)}>Auth</h1>
+        <h1 className={cn("text-3xl font-semibold", font.className)}>👾 Auth</h1>
         <p className="text-muted-foreground text-sm">{label}</p>
     </div>
 }
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,15 +1,83 @@
-import {CardWrapper}  from "@/components/auth/CardWrapper"
+"use client";
 
-export const LoginForm = () => {
+import { CardWrapper } from "@/components/auth/CardWrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { loginSchema } from "@/schemas";
+import { z } from "zod";
 
-    return (
-        <CardWrapper 
-        headerLabel="Welcome Back"
-        backButtonLabel="Don't have an account?"
-        backButtonHref="/auth/register"
-        showSocial
-        >
-          Login Form
-        </CardWrapper>
-    )
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FormError } from "@/components/FormError";
+import { FormSuccess } from "../FormSuccess";
+
+const onSubmitHandler = (values: z.infer<typeof loginSchema>) => {
+  console.log(values);
 }
+export const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <CardWrapper
+      headerLabel="Welcome Back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form
+          className="flex flex-col space-y-6"
+          onSubmit={form.handleSubmit(onSubmitHandler)}
+        >
+          <div className="space-y-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="joe_doe@example.com" {...field} />
+                </FormControl>
+                <FormMessage /> 
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="**********" {...field} />
+                </FormControl>
+                <FormMessage /> 
+              </FormItem>
+            )}
+          ></FormField>
+          </div>
+            <FormError message="" />
+            <FormSuccess message="" />
+             <Button type="submit" className="w-full bg-indigo-900 text-white">
+                Login
+              </Button> 
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
